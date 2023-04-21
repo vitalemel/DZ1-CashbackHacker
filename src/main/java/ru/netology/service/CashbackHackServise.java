176134e96e96d7b186b7dc162754ce1bff46b32d
Vital E <vitalyyemelyanov6@gmail.com>
2023-04-21 14:45:27 +0300

    initial commit

--- src/main/java/ru/netology/service/CashbackHackServise.java
+++
@@ -1,9 +0,0 @@
-package  ru.netology.service;
- public class CashbackHackServise {
-     private final int boundary = 1000;
-
-         public int remain(int amount) {
-             return boundary - amount % boundary;
-         }
-     }
-
---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,7 @@
+package ru.netology.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+}
