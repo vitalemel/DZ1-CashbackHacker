18a052f5c86df1aed3895664522a63834ac9d914
Vital E <vitalyyemelyanov6@gmail.com>
2023-04-21 22:57:49 +0300

    add wrapper

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,8 +4,8 @@
     public class CashbackHackService {
         private final int boundary = 1000;
 
-        public int remain() {
-            int amount = 950;
+        public int remain(int amount) {
+
             return boundary - amount % boundary;
         }
     }
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,15 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-
- public class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
-    public  void testRemain() {
-        CashbackHackService service= new CashbackHackService();
-        int amount =950;
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 950;
         int expected = 50;
-        int actual  = service.remain();
-        Assert.assertEquals(actual,expected);
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int expected = 200;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+
     }
 }
