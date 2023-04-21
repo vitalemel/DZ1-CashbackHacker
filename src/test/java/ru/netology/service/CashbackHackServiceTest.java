3d3e71aeb091267006db2273cbbcfe92ca101acc
Vital E <vitalyyemelyanov6@gmail.com>
2023-04-21 15:55:38 +0300

    add gitignore

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,22 @@
 package ru.netology.service;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class CashbackHackServiceTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+ public class CashbackHackServiceTest {
+    @Test
+    public void testRemain() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 
 }
